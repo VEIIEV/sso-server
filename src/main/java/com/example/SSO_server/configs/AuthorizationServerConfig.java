5c3b29549335212fa23250e4119c85c94837af95
Боткин_Кирилл <worldloflwarcraft@mail.ru>
2024-10-05 19:14:57 +0400

    init commit

---
+++ src/main/java/com/example/SSO_server/configs/AuthorizationServerConfig.java
@@ -0,0 +1,27 @@
+package com.example.SSO_server.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.exceptionHandling(exception ->
+                exception.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+        );
+        return http.build();
+
+    }
+
+}
